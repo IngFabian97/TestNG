931c71e1a35883f647c9b3cf936dfc1fd44d47ff
IngFabian97 <fabian.guarin@hotmail.com>
2025-07-21 17:44:25 -0500

    Add report allure

--- src/test/java/principal/ExcelMonstruoTests.java
+++ src/test/java/principal/ExcelMonstruoTests.java
@@ -10,6 +10,9 @@ import org.testng.annotations.Test;
 import com.testng.modelos.Monstruo;
 import com.testng.reader.ExcelReader;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
 import utilities.BaseTest;
 
 public class ExcelMonstruoTests extends BaseTest{
@@ -22,24 +25,32 @@ public class ExcelMonstruoTests extends BaseTest{
     }
 
     @Test(groups = {"regression", "smoke"})
+    @Description("Verifica el nombre del primer monstruo")
+    @Severity(SeverityLevel.BLOCKER)
     public void primerTest(){
         final var primerMonstruo = monstruos.get(0);
         Assert.assertEquals(primerMonstruo.getNombre(), "TOLOSA", "El nombre del primer monstruo no es el esperado");
     }
 
     @Test(groups = {"regression", "smoke"})
+    @Description("Verifica el número total de monstruos")
+    @Severity(SeverityLevel.CRITICAL)
     public void segundoTest(){
         final var n = monstruos.size();
         Assert.assertEquals(n, 14, "El número de monstruos no es el esperado");
     }
 
     @Test (groups = {"regression"})
+    @Description("Verifica el nivel del tercer monstruo")
+    @Severity(SeverityLevel.NORMAL)
     public void tercerTest(){
         final var tercerMonstruo = monstruos.get(2);
         Assert.assertEquals(tercerMonstruo.getNivel(),22, "El nivel del tercer monstruo no es el esperado");
     }
 
     @Test(groups = {"regression"})
+    @Description("Verifica los detalles del último monstruo")
+    @Severity(SeverityLevel.MINOR)
     public void cuartoTest(){
          final var ultimoMonstruo = monstruos.get(monstruos.size() - 1);
 
@@ -54,6 +65,8 @@ public class ExcelMonstruoTests extends BaseTest{
     }
 
     @Test(dataProvider = data.CustomData.DP_NAME, dataProviderClass = data.CustomData.class, groups = {"regression"})
+    @Description("Verifica la edad y nivel de un monstruo proporcionado por el DataProvider")
+    @Severity(SeverityLevel.TRIVIAL)
     public void quintoTest(Monstruo monstruo){
         softAssert.assertTrue(monstruo.getEdad() > 0, "La edad del monstruo debe ser mayor que 0");
         softAssert.assertTrue(monstruo.getNivel() < 1000, "El nivel del monstruo debe ser menor que 1000");
--- src/test/java/principal/JsonUserTests.java
+++ src/test/java/principal/JsonUserTests.java
@@ -8,6 +8,9 @@ import org.testng.annotations.Test;
 import com.testng.modelos.User;
 import com.testng.reader.JsonReader;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
 import utilities.BaseTest;
 
 public class JsonUserTests extends BaseTest {
@@ -19,23 +22,31 @@ public class JsonUserTests extends BaseTest {
     }
 
     @Test (groups = {"regression", "smoke"})
+    @Description("Verifica que el ID del usuario sea mayor a 0")
+    @Severity(SeverityLevel.BLOCKER)
     public void primerTest() {
         Assert.assertTrue(user.getId() > 0);
 
     }
     
-    @Test(groups = {"regression", "smoke"})   
+    @Test(groups = {"regression", "smoke"})
+    @Description("Verifica la longitud del campo 'name' del usuario")
+    @Severity(SeverityLevel.CRITICAL)   
     public void segundoTest() {
         Assert.assertEquals(user.getAddress().getGeo().getLng(), 71.7478);
     }
 
     @Test(groups = {"regression"})
+    @Description("Verifica que el campo 'company.bs' del usuario tenga más de 10 caracteres")
+    @Severity(SeverityLevel.NORMAL)
     public void tercerTest() {
         System.out.println("El company.bs es: " + user.getCompany().getBs());
         Assert.assertTrue(user.getCompany().getBs().length() > 10);
     }
 
     @Test(groups = {"regression"})
+    @Description("Verifica los detalles del usuario")
+    @Severity(SeverityLevel.MINOR)
     public void cuartoTest() {
         softAssert.assertEquals(user.getName(),"Mrs. Dennis Schulist", "El nombre del usuario no es el esperado");
         softAssert.assertEquals(user.getId(),6, "El ID del usuario no es el esperado");
